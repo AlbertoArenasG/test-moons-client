40c2ceab1411f3b2332634f4bbe1e4c846d6314c
Alberto Arenas <albarenasg@gmail.com>
2021-09-12 17:16:51 -0500

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,82 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Menu
+        </q-item-label>
+
+        <Menu
+          v-for="link in menuLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import Menu from 'components/Menu.vue'
+
+const linksList = [
+  {
+    title: 'Charts',
+    caption: 'quasar.dev',
+    icon: 'eva-pie-chart-outline',
+    link: '/'
+  },
+  {
+    title: 'Chat',
+    caption: 'github.com/quasarframework',
+    icon: 'eva-message-circle-outline',
+    link: '/chat'
+  }
+];
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    Menu
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      menuLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
